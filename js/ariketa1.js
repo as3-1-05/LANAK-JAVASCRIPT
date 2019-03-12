14401d5fc7ff3d309d2ed794c20feea39ba8b3cf
AS3-1-05 <AS3-1-05@uni.lan>
2019-03-12 12:47:12 +0100

    d

--- js/ariketa1.js
+++ js/ariketa1.js
@@ -5,6 +5,6 @@ z = x + y
 
 if (z > 10) {console.log("es mayor que 10")}
 if (z < 10) {console.log("es menor que 10")}
-else () {console.log("es igual que 10")}
+else {console.log("es igual que 10")}
 
 
