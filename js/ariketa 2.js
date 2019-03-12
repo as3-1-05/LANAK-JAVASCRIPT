b9635fe1e729df5090ba580fa44c34eda6431ebd
AS3-1-05 <AS3-1-05@uni.lan>
2019-03-12 12:58:40 +0100

    fdfd

---
+++ js/ariketa 2.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+z = Math.floor(Math.random() * 11):
+
+console.log("El valor de de z es: " + z)
+
+if (z=10) {
+console.log("z es > 10")}
+
+else if (z < 10) {
+console.log("z es < 10 ")}
+
+else if (z == 10) { 
+console.log("z es igual a 10")}
+
+
+
+
