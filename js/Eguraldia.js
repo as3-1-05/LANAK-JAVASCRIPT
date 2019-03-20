6fd090b56fc3d1047e4d1fe2daf4d0bf2faf8048
AS3-1-05 <AS3-1-05@uni.lan>
2019-03-20 10:18:47 +0100

    jk

---
+++ js/Eguraldia.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+Eguraldia = [
+    {Eguna:"Astelehena", Zerua:"Argia", Temperatura:12},
+    {Eguna:"Asteartea", Zerua:"Argia", Temperatura:1},
+    {Eguna:"Asteazkena", Zerua:"Argia", Temperatura:22},
+    {Eguna:"Osteguna", Zerua:"Argia", Temperatura:13},
+    {Eguna:"Ostirala", Zerua:"Argia", Temperatura:18},
+    
+]
+
+console.log(Eguraldia[0].Eguna, Eguraldia[0].Zerua)
+console.log(Eguraldia[1].Eguna, Eguraldia[1].Zerua)
+console.log(Eguraldia[2].Eguna, Eguraldia[2].Zerua)
+console.log(Eguraldia[3].Eguna, Eguraldia[3].Zerua)
+console.log(Eguraldia[4].Eguna, Eguraldia[4].Zerua)
+
+
+var i;
+for (i = 0; i < tiempo.length; i++) {
+    console.log(tiempo[i].dia + "" + tiempo[i].cielo)
+}
---
+++ js/RandomWhile.js
@@ -0,0 +1,22 @@
+
+
+
+z = -10;
+while (z!= 10) {
+z = Math.floor(Math.random() * 51);
+
+console.log("El valor de de z es: " + z)
+
+    if (z > 10) {
+    console.log("z es > 10")
+    }
+
+    else if (z < 10) {
+    console.log("z es < 10 ")
+    }
+
+    else if (z == 10) { 
+    console.log("z es igual a 10")
+    }
+
+}
