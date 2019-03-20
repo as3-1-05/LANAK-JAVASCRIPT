09309d8bbe4c2b91e21f69b5a7c9366621554ef1
AS3-1-05 <AS3-1-05@uni.lan>
2019-03-20 10:33:52 +0100

    gf

---
+++ js/Eguraldia 1.0.js
@@ -0,0 +1,17 @@
+
+
+
+Eguraldia = [
+    {Eguna:"Astelehena", Zerua:"Argia", Temperatura:12},
+    {Eguna:"Asteartea", Zerua:"Argia", Temperatura:1},
+    {Eguna:"Asteazkena", Zerua:"Argia", Temperatura:22},
+    {Eguna:"Osteguna", Zerua:"Argia", Temperatura:13},
+    {Eguna:"Ostirala", Zerua:"Argia", Temperatura:18},
+    
+]
+
+var i;
+for (i = 0; i < tiempo.length; i++) {
+    console.log(tiempo[i].dia + "" + tiempo[i].cielo)
+}
+
